a2fc4fb645792a2aee8c98197e28d8eedeb7687a
anna-golovacheva <anna.ch.ollala@gmail.com>
2022-10-12 01:57:33 +0300

    utils upgraded

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-import csv
-
 from utils import *
 
-data = []
-with open('ads.csv', encoding='utf-8') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        data.append(row)
+def main():
+    data = []
+    with open('ads.csv', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            data.append(row)
 
-del data[0]
+    del data[0]
 
-names = [item[2] for item in data]
-authors_file_name = 'authors.csv'
+    names = [item[2] for item in data]
+    authors_file_name = 'authors.csv'
 
-names_dict = make_dict(names, authors_file_name)
+    names_dict = make_dict(names, authors_file_name)
 
-addresses = [item[5] for item in data]
-addresses_file_name = 'addresses.csv'
+    addresses = [item[5] for item in data]
+    addresses_file_name = 'addresses.csv'
 
-addresses_dict = make_dict(addresses, addresses_file_name)
+    addresses_dict = make_dict(addresses, addresses_file_name)
 
-with open('ads_new.csv', 'w', newline='', encoding='utf-8') as csvfile_ads:
-    writer = csv.writer(csvfile_ads, delimiter=',')
-    for item in data:
-        item[2] = names_dict[item[2]]
-        item[5] = addresses_dict[item[5]]
-        item[1] = item[1].replace('\n', ' ')
-        item[4] = item[4].replace('\n', ' ')
-        writer.writerow(item)
+    with open('ads_new.csv', 'w', newline='', encoding='utf-8') as csvfile_ads:
+        writer = csv.writer(csvfile_ads, delimiter=',')
+        for item in data:
+            item[2] = names_dict[item[2]]
+            item[5] = addresses_dict[item[5]]
+            item[1] = item[1].replace('\n', ' ')
+            item[4] = item[4].replace('\n', ' ')
+            writer.writerow(item)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import csv
 
 def make_dict(items_list, file_name):
+    """
+    Создаёт список уникальных значений (авторов или адресов), записывает их
+    в словарь со значением id и в csv файл.
+    Возвращает словарь, в котором значениями являются id.
+    """
     unique_items = list(set(items_list))
     items_dict = {name: i + 1 for i, name in enumerate(unique_items)}
 
